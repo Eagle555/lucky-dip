0de1fe8f36ab417313b475331b4cf9bd97552f75
Rob Eagleheart <89193999+Eagle555@users.noreply.github.com>
2022-04-22 16:00:40 +0100

    rspec update

--- spec/luckydip_spec.rb
+++ spec/luckydip_spec.rb
@@ -2,5 +2,7 @@ require_relative "../luckydip"
 
 describe LuckyDip, 'test user interface and functionality' do
   luckydip = LuckyDip.new #new object
+  context 'Respond to methods and object' do
 
+  end
 end
