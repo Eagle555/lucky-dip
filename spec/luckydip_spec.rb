3e765107df4419767e6d0b86c5a516de00b7c7cd
Rob Eagleheart <89193999+Eagle555@users.noreply.github.com>
2022-04-26 21:46:25 +0100

    rspec update

--- spec/luckydip_spec.rb
+++ spec/luckydip_spec.rb
@@ -3,8 +3,8 @@ require_relative "../luckydip"
 describe LuckyDip, 'test user interface and functionality' do
   luckydip = LuckyDip.new #new object
   context 'Respond to methods and object' do
-
-
-    
+    it 'Creates new Bank object' do
+      expect(luckydip).to be_instance_of LuckyDip
+    end
   end
 end
